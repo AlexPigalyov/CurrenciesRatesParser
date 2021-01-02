d730cb48a7f83678f71d0717ef36631eb0e1967d
AlexPigalyov <alex.pigalyov@gmail.com>
2021-01-02 19:40:09 +0300

    add quartz scheduler to run parsing every 4.5 minutes

---
+++ CurrenciesRatesParser/CurrenciesRatesParser/CurrenciesRatesParserJob.cs
@@ -0,0 +1,43 @@
+﻿using CurrenciesRatesParser.Model;
+using Newtonsoft.Json;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrenciesRatesParser
+{
+    public class CurrenciesRatesParserJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Task<Dictionary<string, double>> currencyRatesTask = RatesDataHelper.GetCurrencyRates();
+            Task<Dictionary<string, double>> metalRatesTask = RatesDataHelper.GetMetalRates();
+
+            await Task.WhenAll(currencyRatesTask, metalRatesTask);
+
+            Dictionary<string, double> currencyRates = await currencyRatesTask;
+            Dictionary<string, double> metalRates = await metalRatesTask;
+
+            Dictionary<string, double> allRates = new Dictionary<string, double>();
+
+            foreach (KeyValuePair<string, double> metalCurrency in metalRates)
+            {
+                allRates.Add(metalCurrency.Key, metalCurrency.Value);
+            }
+            allRates.Add("USD", 1);
+            foreach (KeyValuePair<string, double> valuteCurrency in currencyRates)
+            {
+                allRates.Add(valuteCurrency.Key, valuteCurrency.Value);
+            }
+
+            RatesDataHelper.AddRates(new Rates()
+            {
+                Date = DateTime.Now,
+                Value = JsonConvert.SerializeObject(allRates)
+            });
+        }
+    }
+}
--- CurrenciesRatesParser/CurrenciesRatesParser/Program.cs
+++ CurrenciesRatesParser/CurrenciesRatesParser/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Quartz;
+using Quartz.Impl;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +13,25 @@ namespace CurrenciesRatesParser
     {
         static void Main(string[] args)
         {
+            
+            ISchedulerFactory schedFact = new StdSchedulerFactory();
 
+            IScheduler sched = schedFact.GetScheduler().Result;
+            sched.Start();
+
+            IJobDetail job = JobBuilder.Create<CurrenciesRatesParserJob>()
+                .WithIdentity("CurrenciesRatesParserJob")
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(60 * 4 + 30)
+                    .RepeatForever())
+                .Build();
+
+            _ = sched.ScheduleJob(job, trigger).Result;
+            Console.ReadLine();
         }
     }
 }
