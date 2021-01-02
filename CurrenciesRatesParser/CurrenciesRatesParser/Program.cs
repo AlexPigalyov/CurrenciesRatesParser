a087f5d676416a40237466b93f74842444b5e204
AlexPigalyov <alex.pigalyov@gmail.com>
2021-01-02 15:29:19 +0300

    Initial commit

---
+++ CurrenciesRatesParser/CurrenciesRatesParser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrenciesRatesParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
