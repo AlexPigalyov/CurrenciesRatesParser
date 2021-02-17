94d22263c48d844ed1a0d92145db7f70fbef97a4
AlexPigalyov <alex.pigalyov@gmail.com>
2021-02-17 16:51:46 +0300

    fix last commit

---
+++ CurrenciesRatesParser/CurrenciesRatesParser/Jobs/CoinRatesParserJob.cs
@@ -0,0 +1,40 @@
+﻿using CurrenciesRatesParser.Model;
+using Quartz;
+using ratesRatesParser.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CurrenciesRatesParser.Jobs
+{
+    public class CoinRatesParserJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Task<List<CoinsRates>> coinsRatesZolotoyZapasTask = ParserService.GetCoinsRatesZolotoyZapas();
+            Task<List<CoinsRates>> coinsRatesZolotoMDTask = ParserService.GetCoinsRatesZolotoMD();
+            Task<List<CoinsRates>> coinsRatesZolotoyClubTask = ParserService.GetCoinsRatesZolotoyClub();
+            Task<List<CoinsRates>> coinsRatesMotenaInvestTask = ParserService.GetCoinsRatesMonetaInvest();
+
+            await Task.WhenAll(
+                coinsRatesZolotoyZapasTask,
+                coinsRatesZolotoyClubTask,
+                coinsRatesZolotoMDTask,
+                coinsRatesMotenaInvestTask);
+
+            List<CoinsRates> coinsRatesZolotoyZapas = await coinsRatesZolotoyZapasTask;
+            List<CoinsRates> coinsRatesZolotoMD = await coinsRatesZolotoMDTask;
+            List<CoinsRates> coinsRatesZolotoyClub = await coinsRatesZolotoyClubTask;
+            List<CoinsRates> coinsRatesMotenaInvest = await coinsRatesMotenaInvestTask;
+
+            CoinsRatesDataHelper.AddCoinsRatesRange(coinsRatesZolotoyZapas);
+            Console.WriteLine("Coins rates zolotoy zapas saved. Time: {0}", DateTime.Now.ToString("HH:mm:ss"));
+            CoinsRatesDataHelper.AddCoinsRatesRange(coinsRatesZolotoMD);
+            Console.WriteLine("Coins rates zoloto md saved. Time: {0}", DateTime.Now.ToString("HH:mm:ss"));
+            CoinsRatesDataHelper.AddCoinsRatesRange(coinsRatesZolotoyClub);
+            Console.WriteLine("Coins rates zolotoy club saved. Time: {0}", DateTime.Now.ToString("HH:mm:ss"));
+            CoinsRatesDataHelper.AddCoinsRatesRange(coinsRatesMotenaInvest);
+            Console.WriteLine("Coins rates moneta invest saved. Time: {0}", DateTime.Now.ToString("HH:mm:ss"));
+        }
+    }
+}
